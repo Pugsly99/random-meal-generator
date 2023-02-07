a6e4af2b0b820e4953f0edc9e6c8d2dae52f9ab2
Ryland Adams <rylandadams@yahoo.com>
2023-02-07 10:41:02 -0800

    button and api setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [recipe, setRecipe] = useState(null);
+
+	const generateRecipe = () => {
+		axios
+			.get('https://www.themealdb.com/api/json/v1/1/random.php')
+			.then(function (response) {
+				console.log(response.data.meals);
+				setRecipe(response.data.meals);
+			})
+			.catch(function (error) {
+				console.log(error);
+			})
+			.finally(function () {
+				console.log('recipe', recipe);
+			});
+	};
+
+	return (
+		<div className='App'>
+			<div className='Header'>
+				<button onClick={generateRecipe}> Generate Recipe </button>
+			</div>
+			<div className='Body'></div>
+		</div>
+	);
 }
 
 export default App;
