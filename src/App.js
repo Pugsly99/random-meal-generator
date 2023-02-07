555da8a62d5b04838ee357f1b41ac4a6f9ac9e98
Ryland Adams <rylandadams@yahoo.com>
2023-02-07 11:29:56 -0800

    content layout bones, Iframe input issues

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
 import React, { useState } from 'react';
 import axios from 'axios';
+import Iframe from 'react-iframe';
 
 function App() {
-	const [recipe, setRecipe] = useState(null);
+	const [recipe, setRecipe] = useState();
 
 	const generateRecipe = () => {
 		axios
 			.get('https://www.themealdb.com/api/json/v1/1/random.php')
 			.then(function (response) {
-				console.log(response.data.meals);
-				setRecipe(response.data.meals);
+				console.log(response.data.meals[0]);
+				setRecipe(response.data.meals[0]);
 			})
 			.catch(function (error) {
 				console.log(error);
-			})
-			.finally(function () {
-				console.log('recipe', recipe);
 			});
+
+		console.log(recipe);
 	};
 
 	return (
@@ -25,7 +25,26 @@ function App() {
 			<div className='Header'>
 				<button onClick={generateRecipe}> Generate Recipe </button>
 			</div>
-			<div className='Body'></div>
+			{recipe ? (
+				<div className='Body'>
+					<img
+						src={recipe.strMealThumb}
+						alt='Recipe Image'
+						className='recipeImage'
+					/>
+					<div className='tags'>//Category //Area //Tags</div>
+					<h1 className='title'></h1>
+					<div className='about'></div>
+					<div className='ingredients'></div>
+					<Iframe
+						url={recipe.strYoutube}
+						width='640px'
+						height='320px'
+					/>
+				</div>
+			) : (
+				<> Lets Get You Somthing To Cook </>
+			)}
 		</div>
 	);
 }
